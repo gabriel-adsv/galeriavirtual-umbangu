1fb50b0e8c8c823acaae98be7fe36c472c75f3b6
IceKirby <IceKirby@users.noreply.github.com>
2022-07-23 17:35:27 -0300

    Texto ao redor das imagens, formatação da biografia e outras coisas pequenas

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 /*  abre e fecha o menu quando clicar no icone: hamburguer e x */
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
+    nav.classList.toggle('show');
+  });
 }
 
 /* quando clicar em um item do menu, esconder o menu */
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll('nav ul li a');
 
 for (const link of links) {
   link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
+    nav.classList.remove('show');
+});
 }
 
 /* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
 
 function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
-    header.classList.add('scroll')
+    header.classList.add('scroll');
   } else {
     // menor que a altura do header
-    header.classList.remove('scroll')
+    header.classList.remove('scroll');
   }
 }
 
@@ -45,7 +45,7 @@ const swiper = new Swiper('.swiper-container', {
       setWrapperSize: true
     }
   }
-})
+});
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
@@ -53,7 +53,7 @@ const scrollReveal = ScrollReveal({
   distance: '30px',
   duration: 700,
   reset: true
-})
+});
 
 scrollReveal.reveal(
   `#inicio .image, #inicio .text,
@@ -64,47 +64,47 @@ scrollReveal.reveal(
   footer .brand, footer .social
   `,
   { interval: 100 }
-)
+);
 
 /* Botão voltar para o topo */
-const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector('.back-to-top');
 
 function backToTop() {
   if (window.scrollY >= 560) {
-    backToTopButton.classList.add('show')
+    backToTopButton.classList.add('show');
   } else {
-    backToTopButton.classList.remove('show')
+    backToTopButton.classList.remove('show');
   }
 }
 
 /* Menu ativo conforme a seção visível na página */
-const sections = document.querySelectorAll('main section[id]')
+const sections = document.querySelectorAll('main section[id]');
 function activateMenuAtCurrentSection() {
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
 
   for (const section of sections) {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
 
-    const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
 
     if (checkpointStart && checkpointEnd) {
       document
         .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.add('active')
+        .classList.add('active');
     } else {
       document
         .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.remove('active')
+        .classList.remove('active');
     }
   }
 }
 
 /* When Scroll */
 window.addEventListener('scroll', function () {
-  changeHeaderWhenScroll()
-  backToTop()
-  activateMenuAtCurrentSection()
-})
+  changeHeaderWhenScroll();
+  backToTop();
+  activateMenuAtCurrentSection();
+});
